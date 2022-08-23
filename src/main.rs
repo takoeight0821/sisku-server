11af32ace88b0c4f78b6356e88ee820bf997357d
Yuya Kono <takohati0821@gmail.com>
2022-08-23 11:15:11 +0900

    Return hovercraft path as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::path::Path;
 
-use actix_files as fs;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{get, App, HttpServer, Responder, web::Json};
+// use actix_files as fs;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let hovercraft_path = Path::new(
+        &std::env::var("XDG_DATA_HOME")
+            .unwrap_or(format!("{}/.local/share", std::env::var("HOME").unwrap())),
+    )
+        .join("sisku")
+        .join("hovercraft");
+    Json(hovercraft_path)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let hovercraft_path = Path::new(
-            &std::env::var("XDG_DATA_HOME")
-                .unwrap_or(format!("{}/.local/share", std::env::var("HOME").unwrap())),
-        )
-        .join("sisku")
-        .join("hovercraft");
-        App::new()
-            .service(fs::Files::new("/hovercraft", &hovercraft_path).show_files_listing())
-            .service(fs::Files::new("/", "./sisku-react/build").show_files_listing())
+        let cors = Cors::permissive();
+        App::new().wrap(cors).service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
