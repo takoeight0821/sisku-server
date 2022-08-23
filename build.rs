73c67e3d3026e39a5c63e5f364d8117902469d1e
Yuya Kono <takohati0821@gmail.com>
2022-08-23 11:15:26 +0900

    Comment out build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
-use std::{env, path::Path, io, process::Command};
+// use std::{env, path::Path, process::Command};
+use std::io;
 
 fn main() -> io::Result<()> {
-    let sisku_react_path = Path::new("./sisku-react");
-    env::set_current_dir(&sisku_react_path)?;
-    Command::new("npm")
-        .args(&["run", "build"])
-        .status()?;
+    // let sisku_react_path = Path::new("./sisku-react");
+    // env::set_current_dir(&sisku_react_path)?;
+    // Command::new("npm").args(&["run", "build"]).status()?;
     Ok(())
 }
